d19f4e3c99b33118693788e42a607817e6f94649
SergeXu <serge.xzj@cainiao.com>
2017-08-24 18:15:11 +0800

    edit for python3

--- guessGame2.py
+++ guessGame2.py
@@ -6,38 +6,39 @@ def guessOnce(num,result):
 #	print "guessOnce: "+str(num)+ " "+str(result)
 	is_right=(int(num) == int(result))
 	if is_right:
-		print "#"*3+"_"*30+"#"*3
-		print "猜正确了!!! 你赢了！！"
-		print "数字是 "+ str(num) 
-		print "#"*3+"_"*30+"#"*3
+		print ("#"*3+"_"*30+"#"*3)
+		print ("猜正确了!!! 你赢了！！")
+		print ("数字是 "+ str(num) )
+		print ("#"*3+"_"*30+"#"*3)
 		return is_right
 	elif (int(num) < int(result)):
-		print "数值太小了"
+		print ("数值太小了")
 		return is_right
 	else: 
-		print "数值太大了"
+		print ("数值太大了")
 		return is_right
-	
+
 def guessStart():
-	print "*"*70
+	print ("*"*70)
 	isRight = False;
-	max_num = raw_input("输入最大值整数范围:\n")
+#	max_num = raw_input("输入最大值整数范围:\n")
+	max_num = input("输入最大值整数范围:\n")
 	while (int(max_num)<=0):
-		print "数字不合法"
-		max_num = raw_input("输入最大值整数范围:\n")	
+		print ("数字不合法")
+		max_num = input("输入最大值整数范围:\n")	
 	return max_num;
 #暂时不用
 def checkMacNum(inputNum,maxNum):
-	print "Guess a number in 0 to "+str(max_num)
-	input = raw_input("")
-	if(input>0 and input< maxNum):
-		return input
+	print ("Guess a number in 0 to "+str(max_num))
+	input_num = input("")
+	if(input_num>0 and input_num< maxNum):
+		return input_num
 	else:
-		print "the number is not fit"
+		print ("the number is not fit")
 
 
 ########### 主流程
-print "猜数字游戏开始："
+print ("猜数字游戏开始：")
 isContinue = True;
 while isContinue:
 	max_num = guessStart()
@@ -45,10 +46,9 @@ while isContinue:
 #	checkMacNum();
 	isRight = False
 	while (not isRight):
-		guess_num = raw_input("猜一个整数: ")
-	 	isRight = guessOnce(guess_num,random_num)
+		guess_num = input("猜一个整数: ")
+		isRight = guessOnce(guess_num,random_num)
 
 
-
-print "exit success"
-print "_"*100
+print ("exit success")
+print ("_"*100)
