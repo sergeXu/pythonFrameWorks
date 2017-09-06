e57c0c6c1683a5c1051e3fb23e30eea036f43e0c
SergeXu <serge.xzj@cainiao.com>
2017-09-06 16:47:15 +0800

    testing?

---
+++ firstPythonProject/cn/serge/console/try/test02.py
@@ -0,0 +1,14 @@
+
+message = "that is a test text"
+print(message)
+
+message = "message changed"
+print(message)
+
+squrs =[]
+for num in list(range(1,11)):
+    squrs.append(num**2)
+print(squrs)
+
+squares = [num**3 for num in range(1,11)]
+print(squares)
